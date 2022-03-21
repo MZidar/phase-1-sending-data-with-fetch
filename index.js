494a78381c6c48c52bdfa03282dfdf271e447bb9
MZidar <MZidar@gmail.com>
2022-03-21 13:36:51 -0400

    commit tree 76366f9dcd25f2bbc1452cc3ffa7d2dc59ec614d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+
+function submitData(userName, userMail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name:userName,
+            email:userMail
+        }),
+    })
+    .then(res => res.json())
+    .then(res => {
+        let body = document.querySelector('body');
+        body.innerHTML = res.id;
+       
+    })
+    .catch(() => {
+        let body = document.querySelector('body');
+        body.innerHTML = "Unauthorized Access";
+    });
+
+
+}
